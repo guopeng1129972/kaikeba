752a9dd8da8aa227a0ad3b74dbeea3a3572d6f93
guopeng1129972 <381694179@qq.com>
2021-05-13 00:38:16 +0800

    模拟vue实现KVue实例
    1.复用  defineReactive observe
    2.实现proxy
    3. 创建Observer类 =>判断响应式是对象还是数组，如果是Object执行walk
    4.proxy之后执行Compile
    5.compile处理node节点
    6.isElement判断是不是节点
    7. isInter判断是不是编译的模板值{{}}

---
+++ 2/1/kvue/kvue.js
@@ -0,0 +1,101 @@
+function defineReactive(obj, key, val) {
+  //递归，如果val 即监听的对象的元素是一个对象，形如obj.baz.n,就先对子元素进行监听
+  observe(val); //get bas; get n
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log("get", key);
+      return val;
+    },
+    set(newVal) {
+      if (newVal !== val) {
+        console.log("set", key);
+        val = newVal;
+        // 当用户直接替换属性时，添加监听
+        observe(newVal);
+      }
+    },
+  });
+}
+// 遍历响应式处理，即处理的响应式很多，需要监听当前处理过程
+function observe(obj) {
+  if (typeof obj !== "object" || typeof obj == null) {
+    return;
+  }
+  //预留对数组的处理，先表示对对象的处理逻辑
+  new Observer(obj);
+}
+// 能够将传入对象中的所有key代理到指定对象上
+function proxy(vm) {
+  Object.keys(vm.$data).forEach((key) =>
+    Object.defineProperty(vm, key, {
+      get() {
+        return vm.$data[key];
+      },
+      set(v) {
+        vm.$data[key] = v;
+      },
+    })
+  );
+}
+class Compile {
+  // 宿主元素与KVue实例
+  constructor(el, vm) {
+    this.$vm = vm;
+    //获取DOM元素
+    this.$el = document.querySelector(el);
+    if (this.$el) {
+      this.compile(this.$el);
+    }
+  }
+  // 遍历node,判断节点类型，做不同处理
+  compile(node) {
+    const childNodes = node.childNodes;
+    Array.from(childNodes).forEach((n) => {
+      // 判断节点类型
+      if (this.isElement(n)) {
+        // 如果子元素也是childNodes需要递归
+        console.log("编译节点", n.nodeName);
+        if (n.childNodes.length > 0) {
+          this.compile(n);
+        }
+      } else if (this.isInter(n)) {
+        console.log("编译文本", n.textContent);
+      }
+    });
+  }
+  isElement(n) {
+    return n.nodeType === 1;
+  }
+  isInter(n) {
+    return n.nodeType === 3 && /\{\{(.*)\}\}/.test(n.textContent);
+  }
+}
+// 监听响应式数据,根据传入的值做相应的处理
+class Observer {
+  constructor(obj) {
+    // 根据传入的值做相应的处理，如果是arr就按照arr的思路去处理响应式，否则使用对象的方法
+    if (Array.isArray(obj)) {
+      console.log("arr todo");
+    } else {
+      this.walk(obj);
+    }
+  }
+  // 这里预留了对如果是数组的话的处理，以及如果是数组的话，添加walk方法处理
+  walk(obj) {
+    Object.keys(obj).forEach((key) => defineReactive(obj, key, obj[key]));
+  }
+}
+
+class KVue {
+  constructor(options) {
+    // 0.保存选项
+    this.$options = options;
+    this.$data = options.data;
+    // 1.对data进行响应式处理
+    observe(options.data);
+    // 2.代理
+    proxy(this);
+    // 3.编译
+    new Compile(options.el, this);
+  }
+}
