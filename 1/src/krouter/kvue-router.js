0161d4d7eeb6c300e94f05470c6a260599b6cb74
guopeng1129972 <381694179@qq.com>
2021-05-22 14:37:02 +0800

    实现响应式更新
    1.在全局的vue实例对象的beforeCreate阶段，混入，将当期的options.router以闭包的逻辑挂载在Vue.prototype.$router上
    监听url的hashchange事件
    2.根据当前的current遍历router表里面的path，根据当前的current值去render
    3.绑定响应式数据this.current,使用
    Vue.util.defineReactive(this, "current", window.location.hash.slice(1));

--- 1/src/krouter/kvue-router.js
+++ 1/src/krouter/kvue-router.js
@@ -4,16 +4,44 @@ class VueRouter {
   // 插件需要实现一个install方法
   constructor(options) {
     Vue;
-    this.options=options
+    this.options = options;
+    // 数据响应式，current必须是响应式的，这样他变化，使用他的组件就会重新render
+    // 如何创造一个响应式数据
+    // 1.借鸡生蛋 - new Vue({data:{current:'/'}})
+    // 2.使用Vue.util.defineReactive(obj,'current','/'); 
+    // this.current = window.location.hash.slice(1) || "/";
+    Vue.util.defineReactive(this, "current", window.location.hash.slice(1));
+    // 监控url变化
+    window.addEventListener("hashchange", () => {
+      this.current = window.location.hash.slice(1);
+    });
   }
 }
 VueRouter.install = function (_Vue) {
   Vue = _Vue;
+  // 注册router实例
+  // 通过全局混入：Vue.mixin({beforeCreate})
+  Vue.mixin({
+    beforeCreate() {
+      // 仅在根组件创建时执行一次
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router;
+      }
+    },
+  });
   // 注册router-view和router-link组件
   Vue.component("router-view", {
     render(h) {
-      //  url=>component
-      return h(null);
+      //  url=>component url获取方式 1.window.location.hash 2.router:this.$router
+      const { current, options } = this.$router;
+      console.log(current, options);
+      let component = null;
+      Array.from(options.routes).forEach((index) => {
+        if (index.path == current) {
+          component = index.component;
+        }
+      });
+      return h(component);
     },
   });
   Vue.component("router-link", {
